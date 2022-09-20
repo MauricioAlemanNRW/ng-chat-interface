cd35ab5faf984cf3bd1f7e73598dfa7f09327e83
Mauro Alemán <mauro.aleman@numrot.com>
2022-09-20 12:06:15 -0500

    se agregan preguntas y respuestas predefinidas

--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-chatbot',
-  templateUrl: './chatbot.component.html',
-  styleUrls: ['./chatbot.component.css']
+  templateUrl: './chatbot.component.html'
 })
 export class ChatbotComponent implements OnInit {
+
+  respuestas = new Map([
+    ['hola', 'Hola, en que necesitas ayuda?'],
+    ['buenos dias', 'Buenos días, en que necesitas ayuda?'],
+    ['buenas tardes', 'Buenas tardes, en que necesitas ayuda?'],
+    ['buenas noches', 'Buenas noches, en que necesitas ayuda?'],
+
+    ['gracias', 'Con gusto! 😊'],
+    ['muchas gracias', 'Siempre es un gusto! 👌'],
+    
+    ['hasta luego', 'Ha sido un placer, hasta luego!'],
+    ['bye', 'Ha sido un placer, hasta luego!'],
+    ['chao', 'Ha sido un placer, hasta luego!'],
+    ['fin', 'Ha sido un placer']
+  ]);
 
   constructor() { }
 
@@ -21,76 +36,93 @@ export class ChatbotComponent implements OnInit {
         let close = chat.querySelector('.close')
         let send = chat.querySelector('.send')
 
+        // abrir el chat 
         toggles.forEach((toggle: any) => {
           toggle.addEventListener('click', () => {
             chat.classList.add('is-active')
           })
         })
 
+        // enviar mensaje
         send.addEventListener('click', () => {
           this.sendMessage()
         })
 
+        // cerrar chat 
         close.addEventListener('click', () => {
           chat.classList.remove('is-active')
         })
 
-        document.onkeydown = function (evt) {
-          evt = evt || window.event;
-
-          var isEscape = false;
-          if ("key" in evt) {
-            isEscape = (evt.key === "Escape" || evt.key === "Esc");
-          } else {
-            isEscape = (evt.keyCode === 27);
-          }
-          if (isEscape) {
-            chat.classList.remove('is-active')
-          }
-
-
-          var isEnter = false;
-          if ("key" in evt) {
-            isEnter = (evt.key === "Enter" || evt.key === "Etr");
-          } else {
-            isEnter = (evt.keyCode === 13);
-          }
-          if (isEnter) {
-            // this.sendMessage()
-          }
-
-
-        };
-
+        // enviar mensaje presionando la tecla enter 
         document.querySelector("#chatInput")?.addEventListener("keyup", (e) => {
-          let keyboardEvent = <KeyboardEvent> e;
+          let keyboardEvent = <KeyboardEvent>e;
           if (keyboardEvent.keyCode === 13) {
             this.sendMessage();
           }
-      });
+        });
 
+        // activar chat 
         window.LIVE_CHAT_UI = true
-
-
       }
     })
 
   }
 
-
   sendMessage() {
-    debugger
-    let msg = (<HTMLInputElement>document.getElementsByClassName('chat-input')[0]).value
+    // recuperar el texto del mensaje
+    let texto = (<HTMLInputElement>document.getElementsByClassName('chat-input')[0]).value
+    let msg = texto.toLowerCase().trim();
+
     if (msg != "") {
-      const Template = '<div class="message"><p class="text">Xmsg</p></div>'
-      document.getElementsByClassName('messages')[0].innerHTML = document.getElementsByClassName('messages')[0].innerHTML + Template.replace('Xmsg', msg)
-      // document.getElementsByClassName('chat-input')[0].value = "" 
-      
-      console.log(document.querySelector("#chatInput"));
+
+      this.generateParrafo(msg, "same");
+
+      let res = this.getBotResponse(msg);
+
+      this.generateParrafo(res, "reply");
+
     }
+  }
+
+  generateParrafo(msg: any, type: string) {
+    // crear el parrafo del mensaje
+    const p = document.createElement("p");
+    p.setAttribute("class", "text");
+    p.innerHTML = msg;
+    // crear el div del mensaje y agregar el parrafo
+    const Template = document.createElement("div");
+
+    if (type == "reply") {
+      Template.setAttribute("class", "message reply");
+    } else {
+      Template.setAttribute("class", "message");
+    }
+
+    Template.appendChild(p);
+
+    // agregar el mensaje al chatbot
+    let box_message = document.getElementById('box_message');
+    box_message?.appendChild(Template);
+
+    // limpiar el input y bajar el scroll
+    (<HTMLInputElement>document.getElementsByClassName('chat-input')[0]).value = "";
     (<HTMLInputElement>document.getElementsByClassName('chat-app_content')[0]).scrollTop = (<HTMLInputElement>document.getElementsByClassName('messages')[0]).offsetHeight
   }
 
+  getBotResponse(quest: any) {
 
+    if (this.respuestas.get(quest)) {
+      return this.respuestas.get(quest)
+    }
+
+    return "No tengo una respuesta para esta pregunta, por favor elije una de las siguientes opciones:"
+    // if(menus.get(input)){
+    //   return "redirect to menu"
+    // }else if(respuestas.get(input)) {
+    //   return respuestas.get(input)
+    // } else {
+    //   return "Lo siento no tengo respuesta para esta petición"
+    // }
+  };
 
 }
